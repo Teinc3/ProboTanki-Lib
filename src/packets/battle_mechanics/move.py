a36b5deed9d6179ac1dc190d896b31e85fe794b1
Teinc3 <s17415@lsc.hk>
2024-11-07 16:47:58 +0000

    Packet testing

--- src/packets/battle_mechanics/move.py
+++ src/packets/battle_mechanics/move.py
@@ -1,16 +1,39 @@
 from codec.custom.movecodec import MoveCodec
 from codec.primitive import IntCodec, ShortCodec
 from packets.abstractpacket import AbstractPacket
-from random import random
-
-from utils.ebytearray import EByteArray
 
 
 class Move(AbstractPacket):
-    last_client_time = 0
-    last_pos_z = 0
+    # last_client_time = 0
+    # last_pos_z = 0
 
     id = 329279865
     description = "Sends your movement data to the server"
     attributes = ['clientTime', 'specificationID', 'movement']
     codecs = [IntCodec, ShortCodec, MoveCodec]
+
+    # def process(self):
+    #     if Move.last_client_time == 0:
+    #         Move.last_client_time = self.object["clientTime"]
+    #         Move.last_pos_z = self.object["movement"]["pos"]["z"]
+    #         return False
+        
+    #     elif self.object["clientTime"] - Move.last_client_time > 6000:
+    #         Move.last_client_time = self.object["clientTime"]
+    #         Move.last_pos_z = self.object["movement"]["pos"]["z"]
+    #         return False
+        
+    #     cur_time = self.object["clientTime"]
+    #     self.object["movement"]["linV"]["z"] = 20
+    #     self.object["movement"]["pos"]["z"] = Move.last_pos_z + 20 * (cur_time - Move.last_client_time) / 1000
+
+    #     Move.last_client_time = cur_time
+    #     Move.last_pos_z = self.object["movement"]["pos"]["z"]
+
+    #     self.deimplement()
+        
+    #     packet_len, _, unencrypted_data = self.wrap()
+    #     self.sockets.server.sendall(EByteArray().write_int(packet_len).write_int(self.id).write(self.protections.c2s.encrypt(unencrypted_data)))
+
+    #     super().process()
+    #     return True
--- src/packets/turrets/railgunshotout.py
+++ src/packets/turrets/railgunshotout.py
@@ -12,18 +12,3 @@ class Railgun_Shot_OUT(AbstractPacket):
     description = "Sends server details about a released railgun shot"
     attributes = ["clientTime", "staticHitPoint", "targets", "targetHitPoints", "incarnationIDs", "targetBodyPositions", "unknown3DVectors"]
     codecs = [IntCodec, Vector3DCodec, VectorStringCodec, VectorVector3DCodec, VectorShortCodec, VectorVector3DCodec, VectorVector3DCodec]
-
-    def process(self):
-        self.object["targets"] *= 3
-        self.object["targetHitPoints"] *= 3
-        self.object["incarnationIDs"] *= 3
-        self.object["targetBodyPositions"] *= 3
-        self.object["unknown3DVectors"] *= 3
-
-        self.deimplement()
-        
-        packet_len, _, unencrypted_data = self.wrap()
-        self.sockets.server.sendall(EByteArray().write_int(packet_len).write_int(self.id).write(self.protections.c2s.encrypt(unencrypted_data)))
-
-        super().process()
-        return
