f015f6b12cac0d2d6a0ab9b5d20590c4a7e6b12f
Teinc3 <s17415@lsc.hk>
2024-11-22 20:38:28 +0000

    Autoretry

--- packets/others/__init__.py
+++ packets/others/__init__.py
@@ -1,15 +1,15 @@
-from lib.packets.others.acceptmission import Accept_Mission
-from lib.packets.others.battleid import Battle_Id
-from lib.packets.others.buyfromshop import Change_Location
-from lib.packets.others.changefreemission import Change_Free_Mission
-from lib.packets.others.changemission import Change_Mission
-from lib.packets.others.changeshoplocation import Buy_From_Shop
-from lib.packets.others.closesettings import Close_Settings
-from lib.packets.others.loadreferral import Load_Referral
-from lib.packets.others.loadsettings import Load_Settings
-from lib.packets.others.notification import Settings_Notification
-from lib.packets.others.shopinfo import Shop_Info
-from lib.packets.others.showmissions import Show_Missions
+from .acceptmission import Accept_Mission
+from .battleid import Battle_Id
+from .buyfromshop import Change_Location
+from .changefreemission import Change_Free_Mission
+from .changemission import Change_Mission
+from .changeshoplocation import Buy_From_Shop
+from .closesettings import Close_Settings
+from .loadreferral import Load_Referral
+from .loadsettings import Load_Settings
+from .notification import Settings_Notification
+from .shopinfo import Shop_Info
+from .showmissions import Show_Missions
 
 __all__ = [
     Show_Missions,
