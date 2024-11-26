57639de78c88d6ce0afa9192e712384f4d876e77
confusedGustas <gustas.antanas2003@gmail.com>
2024-11-26 04:23:17 +0200

    almost done

--- src/bot/processors/battle.py
+++ src/bot/processors/battle.py
@@ -3,18 +3,36 @@ import time
 from bot.enums import ProcessorIDs
 from lib.modules import packetManager
 from .abstractprocessor import AbstractProcessor
+import datetime as date
+
 
 class BattleProcessor(AbstractProcessor):
     processorID = ProcessorIDs.P_BATTLE
 
     def __init__(self, holder):
         super().__init__(holder)
-        
+
+        self._task_thread = None
         self.current_sheep = None
-        self.client_time = None
+        self._client_time = 0
+        self._last_client_time = date.datetime.now()
+        self.is_spawned = False
         self.sheep_list = []
 
         self.start_battle_loop()
+
+    @property
+    def client_time(self):
+        now = date.datetime.now()
+        elapsed_time = (now - self._last_client_time).total_seconds() * 1000
+        self._client_time += int(elapsed_time)
+        self._last_client_time = now
+        return self._client_time
+
+    @client_time.setter
+    def client_time(self, value):
+        self._client_time = value
+        self._last_client_time = date.datetime.now()
 
     def start_battle_loop(self):
         self._task_thread = threading.Thread(target=self.battle_loop)
@@ -27,10 +45,29 @@ class BattleProcessor(AbstractProcessor):
             time.sleep(1.848)
 
     def execute_battle_function(self):
+        if self.is_spawned is False:
+            send_delay_spawn = packetManager.get_packet_by_name('Death_Delay_End')()
+            self.send_packet(send_delay_spawn)
+            print(send_delay_spawn.object)
+
+            time.sleep(3)
+
+            send_spawn = packetManager.get_packet_by_name('Send_Respawn')()
+            self.send_packet(send_spawn)
+            print(send_spawn.object)
+
+            time.sleep(5.5)
+
+            send_end_respawn_packet = packetManager.get_packet_by_name('End_Resp_Fantom')()
+            self.send_packet(send_end_respawn_packet)
+            print(send_end_respawn_packet.object)
+
+            self.is_spawned = True
+
         if self.current_sheep is None:
             return
         current_team = self.current_sheep['team']
-        opposite_team = 1 if current_team == 2 else 2
+        opposite_team = 1 if current_team == 0 else 0
 
         opposite_sheep = next(
             (sheep for sheep in self.sheep_list if sheep['team'] == opposite_team),
@@ -39,12 +76,14 @@ class BattleProcessor(AbstractProcessor):
 
         if opposite_sheep is None:
             return
-        
-        send_smokey_out = packetManager.get_packet_by_name('Smokey_Shot_OUT')()
-        send_smokey_out.object['clientTime'] = self.client_time
-        send_smokey_out.object['hitPoint'] = opposite_sheep['position']
-        send_smokey_out.deimplement()
-        send_smokey_out.sockets.server.sendall(send_smokey_out.wrap())
+
+        if self.client_time is not None:
+            send_smokey_out = packetManager.get_packet_by_name('Smokey_Shot_OUT')()
+            send_smokey_out.object['clientTime'] = self.client_time
+            send_smokey_out.object['hitPoint'] = opposite_sheep['position']
+            send_smokey_out.deimplement()
+            self.send_packet(send_smokey_out)
+            print(send_smokey_out.object)
 
     def process_packets(self):
         packet_data = self.current_packet.object
@@ -66,12 +105,13 @@ class BattleProcessor(AbstractProcessor):
                 'team': team,
                 'position': position
             }
-
         elif self.compare_packet("Kill_Confirm"):
             target = packet_data['target']
             self.sheep_list = [
                 sheep for sheep in self.sheep_list if sheep['username'] != target
             ]
 
-        elif self.compare_packet("Battle_Ping_Sync"):
-            self.client_time = packet_data['latencyInfo']['clientTime']
+        elif self.compare_packet("Battle_Ping_Info"):
+            current_packet = self.current_packet.object
+            self.client_time = current_packet['serverSessionTime']['clientPing']
+            print("Client time: ", self.client_time)
