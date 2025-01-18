416906f27a7435ba2363aa47968beef9aa295d39
Teinc3 <s17415@lsc.hk>
2025-01-18 18:17:14 +0000

    Rearranged accounts in word popularity

--- src/names/gmail_parser.py
+++ src/names/gmail_parser.py
@@ -15,7 +15,7 @@ from googleapiclient.discovery import build
 # 0 -> Load/email registrations (old functionality)
 # 1 -> Fetch "Nickname Change Warning" emails (new functionality)
 # --------------------------
-MODE = 1
+MODE = 0
 
 # If modifying these SCOPES, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/gmail.modify']  # Changed scope to allow modifying emails
