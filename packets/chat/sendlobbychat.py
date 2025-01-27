3385c616cac7b5a4619c295beade93da69bbbe77
Teinc3 <s17415@lsc.hk>
2025-01-27 20:21:19 +0000

    Tracker abstraction rework (discord integration does not work for now)

--- packets/chat/sendlobbychat.py
+++ packets/chat/sendlobbychat.py
@@ -1,5 +1,6 @@
 from lib.codec.complex import StringCodec
 from lib.packets import AbstractPacket
+from lib.packets.status import Subscribe_Status
 
 
 class Send_Lobby_Chat(AbstractPacket):
@@ -7,3 +8,20 @@ class Send_Lobby_Chat(AbstractPacket):
     description = "Sends a chat message to the lobby"
     codecs = [StringCodec, StringCodec]
     attributes = ['username', 'message']
+
+    def process(self):
+        if self.object['message'].startswith('\\'):
+            # Extract the command and arguments
+            command, *args = self.object['message'][1:].split(' ')
+            if command == 'subscribe':
+                # Get argument
+                target = args[0]
+                # Subscribe to the target
+                packet = Subscribe_Status()
+                packet.objects = [target]
+                self.sockets.server.sendall(packet.wrap(self.protections.c2s))
+
+            return True # Do not forward the packet to the server
+        
+        super().process()
+        return False
