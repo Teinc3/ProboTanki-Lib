83dbabbee8d3a6d34a5bd4d3f6bc1404be67be7e
Teinc3 <s17415@lsc.hk>
2025-01-30 02:48:29 +0000

    Refactor database handling, update proxy settings, and enhance logging for Watchdog integration

--- modules/__init__.py
+++ modules/__init__.py
@@ -3,11 +3,13 @@ from .packetmanager import packetManager, PacketManager
 from .protection import Protection
 from .tankisocket import TankiSocket
 from .abstractprocessor import AbstractProcessor, LayoutID
+from .dbmanager import DBManager
 
 __all__ = [
     logger, Logger,
     packetManager, PacketManager,
     Protection,
     TankiSocket,
-    AbstractProcessor, LayoutID
+    AbstractProcessor, LayoutID,
+    DBManager
 ]
