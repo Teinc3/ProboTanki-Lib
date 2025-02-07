4dc5c328b4996a9ca8c23d435216b2fc94edb377
Teinc3 <s17415@lsc.hk>
2025-02-07 00:39:21 +0000

    Completed notifier cog

--- packets/chat/__init__.py
+++ packets/chat/__init__.py
@@ -1,5 +1,6 @@
 from .receivegamechat import Receive_Game_Chat
 from .receivelobbychat import Receive_Lobby_Chat
+from .receivegamesystemchat import Receive_Game_System_Chat
 from .sendgamechat import Send_Game_Chat
 from .sendlobbychat import Send_Lobby_Chat
 from .wipelobbymessages import Wipe_Lobby_Messages
@@ -9,5 +10,6 @@ __all__ = [
     'Receive_Lobby_Chat',
     'Send_Game_Chat',
     'Receive_Game_Chat',
+    'Receive_Game_System_Chat',
     'Wipe_Lobby_Messages'
 ]
