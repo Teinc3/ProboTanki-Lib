75e070c201211700c82a7064eae9fdb137ddb6f3
Teinc3 <s17415@lsc.hk>
2025-02-08 19:42:31 +0000

    Added custom commands on tanki end extending abstract command message class in library

--- utils/enums/compare.py
+++ utils/enums/compare.py
@@ -2,4 +2,4 @@ from enum import Enum
 
 
 def compare_enums(enum1: Enum, enum2: Enum) -> bool:
-    return enum1.__name__ == enum2.__name__ and enum1.value == enum2.value
+    return enum1.name == enum2.name and enum1.value == enum2.value
