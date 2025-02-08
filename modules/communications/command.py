dd94b914f57b6e4388c6543328d059a77b927395
Teinc3 <s17415@lsc.hk>
2025-02-08 19:42:31 +0000

    Added custom commands on tanki end extending abstract command message class in library

--- modules/communications/command.py
+++ modules/communications/command.py
@@ -1,16 +1,27 @@
+from abc import ABC
+from typing import Generic, TypeVar
+
 from .abstractmessage import AbstractMessage
 from ...utils.enums import MessageType
 
 
-class CommandMessage(AbstractMessage):
+TCommand = TypeVar('TCommand')
+TPayload = TypeVar('TPayload')
+
+class CommandMessage(AbstractMessage, Generic[TCommand, TPayload], ABC):
     """
     A message object that encompasses a command.
 
     Attributes:
-    command -- The command to be executed
+    command: Generic[TCommand] - The command to be executed.
+    payload: Generic[TPayload] - The payload to be passed to the command.
+
+    Generic Types:
+    TCommand - Datatype differentiating the command to be executed
+    TPayload - Structure specifying the payload to be passed to the command
     """
 
-    def __init__(self, command: str, payload: dict):
+    def __init__(self, command: TCommand, payload: TPayload):
         self.command = command
         self.payload = payload
 
