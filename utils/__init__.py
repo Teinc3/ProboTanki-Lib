66715aa22383c955f48e68400d005ede265a59fd
Teinc3 <s17415@lsc.hk>
2025-02-11 13:11:20 +0000

    Removed namescraper from library

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,5 +1,4 @@
 from .address import Address
 from .ebytearray import EByteArray
-from .staffscraper import NameScraper
 from .normalizer import normalize_name
 from .reconnectionconfig import ReconnectionConfig
