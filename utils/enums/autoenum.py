89aa22743ea717f55b3b4a7c9ba4843ca5e76ea9
Teinc3 <s17415@lsc.hk>
2025-02-21 03:18:28 +0000

    Updated autoenum to return str type for values

--- utils/enums/autoenum.py
+++ utils/enums/autoenum.py
@@ -2,7 +2,7 @@ from enum import Enum, auto
 
 
 class AutoEnum(Enum):
-    def _generate_next_value_(name, _start, _count, _last_values):
+    def _generate_next_value_(name, *_) -> str:
         return name
 
 # Exporting auto so we don't have to import it from enum again when we use this 
