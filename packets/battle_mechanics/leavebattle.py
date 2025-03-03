9f3ac6fd16c1f552f90632b5c2358745320dc7d8
Teinc3 <s17415@lsc.hk>
2025-03-03 09:45:23 +0000

    Swapping signs is peak autism

--- packets/battle_mechanics/leavebattle.py
+++ packets/battle_mechanics/leavebattle.py
@@ -3,7 +3,7 @@ from ...packets import AbstractPacket
 
 
 class Leave_Battle(AbstractPacket):
-    id = -377959142
+    id = 377959142
     description = "Leaves battle to a layout (0 = Lobby, 1 = Garage)"
     attributes = ['layout']
     codecs = [IntCodec]
