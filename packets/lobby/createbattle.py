8c168e63a698b13c609cf0b7db60612443cf64f7
Teinc3 <s17415@lsc.hk>
2025-03-30 11:46:49 +0800

    Added new battle creation packet attributes

--- packets/lobby/createbattle.py
+++ packets/lobby/createbattle.py
@@ -9,10 +9,14 @@ class Create_Battle(AbstractPacket):
     description = 'Creates a new battle'
     attributes = ["autoBalance", "battleMode", "format", "friendlyFire", "battleLimits", "mapID",
                   "maxPeopleCount", "name", "parkourMode", "privateBattle", "proBattle", "rankRange",
-                  "noRearm", "theme", "noSupplyBoxes", "noCrystalBoxes", "noSupplies", "noUpgrade"]
+                  "noRearm", "theme", "noSupplyBoxes", "noCrystalBoxes", "noSupplies", "noUpgrade",
+                  "lowResistance", "useDropTimings", "noGoldBoxes", "noGoldSiren", "noGoldDropZone",
+                  "noMedkit", "noMines", "randomGoldChance", "dependentCooldown"]
     codecs = [BoolCodec, IntCodec, IntCodec, BoolCodec, BattleLimitsCodec, StringCodec,
               IntCodec, StringCodec, BoolCodec, BoolCodec, BoolCodec, RankRangeCodec,
-              BoolCodec, IntCodec, BoolCodec, BoolCodec, BoolCodec, BoolCodec]
+              BoolCodec, IntCodec, BoolCodec, BoolCodec, BoolCodec, BoolCodec,
+              BoolCodec, BoolCodec, BoolCodec, BoolCodec, BoolCodec,
+              BoolCodec, BoolCodec, BoolCodec, BoolCodec]
 
     # The "decode" method (s2c) does not have the noUpgrade attribute
     # But since this packet is C2S, we have to include the noUpgrade attribute in the decode method
