4b5e04f07b029a62c6cf516c8a89b7d69046a3a2
Teinc3 <teinc3@gmail.com>
2025-11-14 09:59:19 +0000

    Updated mission resource to longcodec

--- codec/custom/missioncodec.py
+++ codec/custom/missioncodec.py
@@ -1,11 +1,11 @@
 from ..custombasecodec import CustomBaseCodec
 from ..complex import StringCodec
 from ..factory import VectorCodecFactory
-from ..primitive import BoolCodec, IntCodec
+from ..primitive import BoolCodec, IntCodec, LongCodec
 from .missionrewardcodec import MissionRewardCodec
 
 
 class MissionCodec(CustomBaseCodec):
     attributes = ["freeChange", "description", "threshold", "image", "rewards", "progress", "missionID", "changeCost"]
-    codecs = [BoolCodec, StringCodec, IntCodec, IntCodec, VectorCodecFactory(dict, MissionRewardCodec), IntCodec,
+    codecs = [BoolCodec, StringCodec, IntCodec, LongCodec, VectorCodecFactory(dict, MissionRewardCodec), IntCodec,
               IntCodec, IntCodec]
