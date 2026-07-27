f6e3eebd58bab600cb2d161aa1f48a82ceb97952
giannis <giannibbboo@gmail.com>
2026-07-27 05:26:49 +0300

    Update __init__.py

--- packets/clans/__init__.py
+++ packets/clans/__init__.py
@@ -7,10 +7,16 @@ from .claninfoin import Clan_Info_In
 from .clanleaderboardrequest import Clan_Leaderboard_Request
 from .clanleaderboardin import Clan_Leaderboard_In
 
+# Clan profile (view another clan by tag) related packets
+from .clanprofilerequest import Clan_Profile_Request
+from .clanprofilein import Clan_Profile_In
+
 __all__ = [
     Open_Clan_Menu,
     Close_Clan_Menu,
     Clan_Info_In,
     Clan_Leaderboard_Request,
     Clan_Leaderboard_In,
+    Clan_Profile_Request,
+    Clan_Profile_In,
 ]
