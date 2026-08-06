d4760f550fb4ff95f7ab875f69a7d551bf1407e6
giannis <giannibbboo@gmail.com>
2026-08-06 18:07:47 +0300

    Update clanprofilein.py

--- packets/clans/clanprofilein.py
+++ packets/clans/clanprofilein.py
@@ -1,9 +1,7 @@
 from ...packets import AbstractPacket
 from ...codec.custom import ClanProfileCodec
 
-
 class Clan_Profile_In(AbstractPacket):
-
     id = -1855118498
     description = 'Clan Profile'
     attributes = ['clan']
