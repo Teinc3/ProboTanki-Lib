7da4c38ade6be7ebeab69a941df1fffb9cb60d9b
giannis <giannibbboo@gmail.com>
2026-08-06 18:08:03 +0300

    Update clanprofilerequest.py

--- packets/clans/clanprofilerequest.py
+++ packets/clans/clanprofilerequest.py
@@ -2,9 +2,7 @@ from ...packets import AbstractPacket
 from ...codec.complex import PascalStringCodec
 from ...codec.primitive import IntCodec
 
-
 class Clan_Profile_Request(AbstractPacket):
-
     id = 947733823
     description = 'Clan Profile Request'
     attributes = ['unknownInt0', 'tag']
